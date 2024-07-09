b5a94499c8e75717f946a6caa505794dfe78253a
cowboyHat123111 <cowboy.hat1@protonmail.com>
2024-07-09 17:25:52 +0300

    refactor components

---
+++ Even or Odd/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Even_or_Odd
+{
+    internal class Program
+    {
+        public static string EvenOrOdd(int number) => number % 2 == 0 ? "Even" : "Odd";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(EvenOrOdd(6));
+        }
+    }
+}
